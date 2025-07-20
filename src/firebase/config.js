83d1369936d06ec2f426435608f151b61f770c4b
unknown <vrehemanth@gmail.com>
2025-07-20 19:38:28 +0530

    Initial commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,17 @@
+// File: src/firebase/config.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGMxzsxSU3IGHY2wZfpe_jWpnj5LbgOYU",
-  authDomain: "catering-reserve.firebaseapp.com",
-  projectId: "catering-reserve",
-  storageBucket: "catering-reserve.firebasestorage.app",
-  messagingSenderId: "762387931179",
-  appId: "1:762387931179:web:909397e44d9eeee3584a6e",
-  measurementId: "G-40C43F855V"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
